fd0361d7ae30305899eee6ef7c19e2407e14f1dd
Ayush Mishra <ayushmishra11182@gmail.com>
2017-02-18 00:47:49 +0530

    Added new test image - test2:caprio face

--- app/src/main/java/com/example/hp/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/hp/opencvtest/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.hp.opencvtest;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -24,26 +17,17 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("opencv_java");
         System.loadLibrary("nonfree");
     }
-
     private ImageView imageView;
     private Bitmap inputImage; // make bitmap from image resource
     private FeatureDetector detector = FeatureDetector.create(FeatureDetector.SIFT);
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        inputImage = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        inputImage = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
         setContentView(R.layout.activity_main);
         imageView = (ImageView) this.findViewById(R.id.imageView);
         sift();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -62,39 +46,4 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageBitmap(inputImage);
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
