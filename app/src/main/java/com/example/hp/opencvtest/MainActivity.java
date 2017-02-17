914a85e20bbf749b2b64c02c445f9624e635cc9d
Ayush Mishra <ayushmishra11182@gmail.com>
2017-02-17 23:02:59 +0530

    Edges detected on a test image. Sift Algo.

---
+++ app/src/main/java/com/example/hp/opencvtest/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.hp.opencvtest;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("opencv_java");
+        System.loadLibrary("nonfree");
+    }
+
+    private ImageView imageView;
+    private Bitmap inputImage; // make bitmap from image resource
+    private FeatureDetector detector = FeatureDetector.create(FeatureDetector.SIFT);
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        inputImage = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+        sift();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void sift() {
+        Mat rgba = new Mat();
+        Utils.bitmapToMat(inputImage, rgba);
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        Imgproc.cvtColor(rgba, rgba, Imgproc.COLOR_RGBA2GRAY);
+        detector.detect(rgba, keyPoints);
+        Features2d.drawKeypoints(rgba, keyPoints, rgba);
+        Utils.matToBitmap(rgba, inputImage);
+        imageView.setImageBitmap(inputImage);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
