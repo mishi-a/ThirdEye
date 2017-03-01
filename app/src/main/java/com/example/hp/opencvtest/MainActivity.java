13901651085a39cef8b6a2f433351ab73e0ce711
Ayush Mishra <ayushmishra11182@gmail.com>
2017-03-01 19:07:53 +0530

    Made the use of feature detector by orb algorithm.. Added New activity with basic interface which calls ORB_feature_detector

--- app/src/main/java/com/example/hp/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/hp/opencvtest/MainActivity.java
@@ -1,208 +1,111 @@
 package com.example.hp.opencvtest;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.SurfaceView;
-import android.view.WindowManager;
-import android.widget.TextView;
+import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.DMatch;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.Scalar;
-import org.opencv.features2d.DescriptorExtractor;
-import org.opencv.features2d.DescriptorMatcher;
-import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.Features2d;
-import org.opencv.imgproc.Imgproc;
+import java.util.Locale;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+    TextToSpeech t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)this.findViewById(R.id.imageView1);
 
-
-public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
-
-    private static final String TAG = "OCVSample::Activity";
-    private int w, h;
-    private CameraBridgeViewBase mOpenCvCameraView;
-    TextView tvName;
-    Scalar RED = new Scalar(255, 0, 0);
-    Scalar GREEN = new Scalar(0, 255, 0);
-    FeatureDetector detector;
-    DescriptorExtractor descriptor;
-    DescriptorMatcher matcher;
-    Mat descriptors2,descriptors1;
-    Mat img1;
-    MatOfKeyPoint keypoints1,keypoints2;
-
-    static {
-        if (!OpenCVLoader.initDebug())
-            Log.d("ERROR", "Unable to load OpenCV");
-        else
-            Log.d("SUCCESS", "OpenCV loaded");
-    }
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    mOpenCvCameraView.enableView();
-                    try {
-                        initializeOpenCVDependencies();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    Locale locale = new Locale("en", "IN");
+                    int availability = t1.isLanguageAvailable(locale);
+                    switch (availability) {
+                        case TextToSpeech.LANG_NOT_SUPPORTED: {
+                            t1.setLanguage(Locale.UK);
+                            break;
+                        }
+                        case TextToSpeech.LANG_MISSING_DATA: {
+                            t1.setLanguage(Locale.UK);
+                            break;
+                        }
+                        case TextToSpeech.LANG_AVAILABLE: {
+                            t1.setLanguage(Locale.UK);
+                            break;
+                        }
+                        case TextToSpeech.LANG_COUNTRY_AVAILABLE:
+                        case TextToSpeech.LANG_COUNTRY_VAR_AVAILABLE: {
+                            t1.setLanguage(locale);
+                            break;
+                        }
                     }
                 }
-                break;
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
             }
+        });
+    }
+    public void  search(View v){
+        EditText g = (EditText) findViewById(R.id.newText);
+        String x = g.getText().toString();
+        if (x.matches("")) {
+            Toast.makeText(this, "You did not enter anything", Toast.LENGTH_SHORT).show();
+            return;
         }
-    };
-
-    private void initializeOpenCVDependencies() throws IOException {
-        mOpenCvCameraView.enableView();
-        detector = FeatureDetector.create(FeatureDetector.ORB);
-        descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
-        matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
-        img1 = new Mat();
-        AssetManager assetManager = getAssets();
-        InputStream istr = assetManager.open("a.jpeg");
-        Bitmap bitmap = BitmapFactory.decodeStream(istr);
-        Utils.bitmapToMat(bitmap, img1);
-        Imgproc.cvtColor(img1, img1, Imgproc.COLOR_RGB2GRAY);
-        img1.convertTo(img1, 0); //converting the image to match with the type of the cameras image
-        descriptors1 = new Mat();
-        keypoints1 = new MatOfKeyPoint();
-        detector.detect(img1, keypoints1);
-        descriptor.compute(img1, keypoints1, descriptors1);
-
+        Uri uri = Uri.parse("http://www.google.com/#q=" + x);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
-
-
-    public MainActivity() {
-
-        Log.i(TAG, "Instantiated new " + this.getClass());
+    public void  feature(View v){
+        Intent intent = new Intent(this,ORB_Feature_Detector.class);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
-
-    /**
-     * Called when the activity is first created.
-     */
+    public void  voice(View v){
+        EditText g = (EditText) findViewById(R.id.newText);
+        String x = g.getText().toString();
+        if (x.matches("")) {
+            Toast.makeText(this, "You did not enter anything", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(getApplicationContext(), x,Toast.LENGTH_SHORT).show();
+        t1.speak(x, TextToSpeech.QUEUE_FLUSH, null);
+        //t1.speak(x.toCharArray(),TextToSpeech.QUEUE_FLUSH, null,null);
+        /*MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.audiocheck);
+        Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();*/
+    }
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        Log.i(TAG, "called onCreate");
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.activity_main);
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-        tvName = (TextView) findViewById(R.id.text1);
-
-    }
-
-    @Override
-    public void onPause() {
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
         super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+    void  click(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+        }
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
         }
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
 
-    public void onCameraViewStarted(int width, int height) {
-        w = width;
-        h = height;
-    }
-
-    public void onCameraViewStopped() {
-        img1.release();
-    }
-
-    public Mat recognize(Mat aInputFrame) {
-
-        Imgproc.cvtColor(aInputFrame, aInputFrame, Imgproc.COLOR_RGB2GRAY);
-        descriptors2 = new Mat();
-        keypoints2 = new MatOfKeyPoint();
-        detector.detect(aInputFrame, keypoints2);
-        descriptor.compute(aInputFrame, keypoints2, descriptors2);
-
-        // Matching
-        MatOfDMatch matches = new MatOfDMatch();
-        if (descriptors1.type() == descriptors2.type() && descriptors1.cols() == descriptors2.cols()) {
-            matcher.match(descriptors1, descriptors2, matches);
-        } else {
-            return aInputFrame;
-        }
-        List<DMatch> matchesList = matches.toList();
-
-        Double max_dist = 0.0;
-        Double min_dist = 100.0;
-
-        for (int i = 0; i < matchesList.size(); i++) {
-            Double dist = (double) matchesList.get(i).distance;
-            if (dist < min_dist)
-                min_dist = dist;
-            if (dist > max_dist)
-                max_dist = dist;
-        }
-
-        LinkedList<DMatch> good_matches = new LinkedList<DMatch>();
-        for (int i = 0; i < matchesList.size(); i++) {
-            if (matchesList.get(i).distance <= (1.5 * min_dist))
-                good_matches.addLast(matchesList.get(i));
-        }
-
-        MatOfDMatch goodMatches = new MatOfDMatch();
-        goodMatches.fromList(good_matches);
-        Mat outputImg = new Mat();
-        MatOfByte drawnMatches = new MatOfByte();
-        if (aInputFrame.empty() || aInputFrame.cols() < 1 || aInputFrame.rows() < 1) {
-            return aInputFrame;
-        }
-        Features2d.drawMatches(img1, keypoints1, aInputFrame, keypoints2, goodMatches, outputImg, GREEN, RED, drawnMatches, Features2d.NOT_DRAW_SINGLE_POINTS);
-        Imgproc.resize(outputImg, outputImg, aInputFrame.size());
-
-        return outputImg;
-    }
-
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        return recognize(inputFrame.rgba());
-
-    }
 }
